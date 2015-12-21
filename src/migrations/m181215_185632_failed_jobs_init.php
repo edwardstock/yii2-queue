19d047f16b9b110be1decda9518b09a71da658e5
Eduard Maximovich <edward.vstock@gmail.com>
2015-12-21 12:53:33 +0300

    фикс миграции

--- src/migrations/m181215_185632_failed_jobs_init.php
+++ src/migrations/m181215_185632_failed_jobs_init.php
@@ -8,9 +8,9 @@ class m181215_185632_failed_jobs_init extends Migration
 		$this->createTable('failed_queue_jobs', [
 			'id'       => Schema::TYPE_BIGPK,
 			'class'    => Schema::TYPE_STRING,
-			'tries'    => Schema::TYPE_STRING,
+			'tries'    => Schema::TYPE_SMALLINT,
 			'log_time' => Schema::TYPE_DOUBLE,
-			'payload'  => Schema::TYPE_INTEGER,
+			'payload'  => Schema::TYPE_TEXT,
 		]);
 	}
 
